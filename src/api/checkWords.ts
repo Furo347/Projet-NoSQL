d545892fe0371fd519441951a3a1afa199b1d354
selogurain12 <lora80123@gmail.com>
2024-03-18 13:32:32 +0100

    add file

--- src/api/checkWords.ts
+++ src/api/checkWords.ts
@@ -8,12 +8,22 @@ function getRandomInt(max: number) {
     return Math.floor(Math.random() * max);
 }
 
+export function randomLetter() {
+    const ranNumber = getRandomInt(25);
+    const letter = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const randomLowerCaseLetter = letter[ranNumber];
+    console.log(randomLowerCaseLetter);
+    return randomLowerCaseLetter.toUpperCase();
+}
+
 const ranNumber = getRandomInt(25);
 const letter = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 const CURRENT_LETTER = letter[ranNumber];
-async function getTheme(word: string) {
+export async function getTheme(word: string) {
     const url = `https://twinword-twinword-bundle-v1.p.rapidapi.com/word_theme/?entry=${word}`;
     const options = {
         method: 'GET',
@@ -29,17 +39,20 @@ async function getTheme(word: string) {
             throw new Error('Failed to fetch data from Twinword API');
         }
         const result = await response.json();
+        console.log(result)
         return result.theme as Array<string>;
     } catch (error) {
         return error;
     }
 }
-
+const theme= getTheme("avion");
+console.log(theme)
 export async function checkWord(word: string, desiredTheme: Themes) {
     let check = false;
     if (word[0] === CURRENT_LETTER) {
         const theme = await getTheme(word) as Array<string>;
         check = theme.includes(desiredTheme);
     }
+    console.log(check)
     return check
 }
--- src/compoments/grid.tsx
+++ src/compoments/grid.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { Button } from '@material-ui/core';
+import { Themes, checkWord, getTheme, randomLetter } from '../api/checkWords';
+import TextField from '@mui/material/TextField';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -16,42 +20,99 @@ const useStyles = makeStyles((theme: Theme) =>
       textAlign: 'center',
       backgroundColor: "#1e90ff"
     },
+    divbutton: {
+      padding: "1rem",
+      justifyContent: "end",
+      display: "flex", 
+    },
+    button: {
+      backgroundColor: "#1e90ff",
+      margin: "2px"
+    },
+    customTextField: {
+      width: '100%',
+    },
+    column: {
+      border: "1px solid",
+      borderRadius: "0px"
+    }
   }),
 );
 
 export default function BacGrid() {
   const classes = useStyles();
+  const [letters, setLetters] = useState<string[]>([]);
+  const [rowData, setRowData] = useState<string[][]>([]); // Utilisez un tableau bidimensionnel pour stocker les données de chaque ligne
+
+  const handleRandomLetter = () => {
+    const newLetter = randomLetter();
+    setLetters(prevLetters => [...prevLetters, newLetter]);
+    // Ajoute un nouvel élément vide à rowData pour la nouvelle ligne
+    setRowData(prevRowData => [...prevRowData, Array.from({ length: 4 }, () => "")]);
+  };
+
+  const handleTextFieldChange = (rowIndex: number, colIndex: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newData = [...rowData];
+    newData[rowIndex][colIndex] = event.target.value;
+    setRowData(newData);
+  };
+
+  const handleValidateLine = () => {
+    // Récupère les données de la ligne actuelle
+    const currentLineData = rowData[rowData.length - 1];
+    console.log("Données de la ligne actuelle :", currentLineData);
+  };
 
   return (
-    <div>
-      <Grid container>
-        <Grid item xs={12}>
-          <Paper className={classes.title}>Jeu du baccalauréat</Paper>
+    <>
+      <div className={classes.divbutton}>
+        <Button className={classes.button} onClick={handleValidateLine}>
+          VALIDER LA LIGNE
+        </Button>
+        <Button className={classes.button} onClick={handleRandomLetter}>
+          TIRER UNE LETTRE
+        </Button>
+      </div>
+      <div>
+        <Grid container alignItems="stretch">
+          <Grid item xs={12}>
+            <Paper className={classes.title}>Jeu du baccalauréat</Paper>
+          </Grid>
+          <Grid container>
+            <Grid item style={{ flexBasis: '12%' }}>
+              <Paper className={classes.paper}>Lettre</Paper>
+            </Grid>
+            <Grid item style={{ flexBasis: '22%' }}>
+              <Paper className={classes.paper}>Vetement</Paper>
+            </Grid>
+            <Grid item style={{ flexBasis: '22%' }}>
+              <Paper className={classes.paper}>Metier</Paper>
+            </Grid>
+            <Grid item style={{ flexBasis: '22%' }}>
+              <Paper className={classes.paper}>Animal</Paper>
+            </Grid>
+            <Grid item style={{ flexBasis: '22%' }}>
+              <Paper className={classes.paper}>Nourriture</Paper>
+            </Grid>
+          </Grid>
+          {letters.map((letter, index) => (
+            <Grid container key={index}>
+              <Grid item style={{ flexBasis: '12%' }}>
+                <Paper className={classes.paper}>{letter}</Paper>
+              </Grid>
+              {[...Array(4)].map((_, i) => (
+                <Grid item key={i} className={classes.column} style={{ flexBasis: '22%' }}>
+                <TextField
+                  className={`${classes.paper} ${classes.customTextField}`}
+                  value={rowData[index] ? rowData[index][i] : ""}
+                  onChange={handleTextFieldChange(index, i)}
+                />
+              </Grid>
+              ))}
+            </Grid>
+          ))}
         </Grid>
-        <Grid container>
-          <Grid item style={{ flexBasis: '10%' }}>
-            <Paper className={classes.paper}>Prénom</Paper>
-          </Grid>
-          <Grid item style={{ flexBasis: '15%' }}>
-            <Paper className={classes.paper}>Prénom</Paper>
-          </Grid>
-          <Grid item style={{ flexBasis: '15%' }}>
-            <Paper className={classes.paper}>Objet</Paper>
-          </Grid>
-          <Grid item style={{ flexBasis: '15%' }}>
-            <Paper className={classes.paper}>Animal</Paper>
-          </Grid>
-          <Grid item style={{ flexBasis: '15%' }}>
-            <Paper className={classes.paper}>Ville ou pays</Paper>
-          </Grid>
-          <Grid item style={{ flexBasis: '15%' }}>
-            <Paper className={classes.paper}>Métier</Paper>
-          </Grid>
-          <Grid item style={{ flexBasis: '15%' }}>
-            <Paper className={classes.paper}>Fruit ou légume</Paper>
-          </Grid>
-        </Grid>
-      </Grid>
-    </div>
+      </div>
+    </>
   );
 }
