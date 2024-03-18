5477d77d5a18427ac1e37f24ee86fdad86cdc640
DawnWT <dwnssb@gmail.com>
2024-03-18 11:26:52 +0100

    feat: refactor api routes

--- src/api/word.ts
+++ src/api/word.ts
@@ -1,9 +1,8 @@
-export enum Themes {
-    Vetement = 'clothing',
-    Metier = 'job',
-    Animal = 'species',
-    Nourriture = 'food'
-}
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { z } from "zod";
+import { Themes } from "../types/rapidapi";
+
 function getRandomInt(max: number) {
     return Math.floor(Math.random() * max);
 }
@@ -12,7 +11,7 @@ const ranNumber = getRandomInt(25);
 const letter = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-export const CURRENT_LETTER = letter[ranNumber];
+const CURRENT_LETTER = letter[ranNumber];
 async function getTheme(word: string) {
     const url = `https://twinword-twinword-bundle-v1.p.rapidapi.com/theme/?entry=${word}`;
     const options = {
@@ -35,7 +34,7 @@ async function getTheme(word: string) {
     }
 }
 
-export async function checkWord(word: string, desiredTheme: Themes) {
+async function checkWord(word: string, desiredTheme: Themes) {
     let check = false;
     if (word[0] === CURRENT_LETTER) {
         const theme = await getTheme(word) as Array<string>;
@@ -46,3 +45,15 @@ export async function checkWord(word: string, desiredTheme: Themes) {
     }
     return check
 }
+
+const word = new Hono()
+    .post('/checkWord', zValidator('json', z.object({
+        word: z.string(),
+        theme: z.nativeEnum(Themes)
+    })), async (ctx) => {
+        const { word, theme } = ctx.req.valid('json');
+        const result = await checkWord(word, theme as Themes);
+        return ctx.json(result);
+    });
+
+export default word
---
+++ src/types/rapidapi.ts
@@ -0,0 +1,6 @@
+export enum Themes {
+  Vetement = 'clothing',
+  Metier = 'job',
+  Animal = 'species',
+  Nourriture = 'food'
+}
