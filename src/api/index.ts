143389c08975ebc237ae35466aa1195712a2e9cf
DawnWT <dwnssb@gmail.com>
2024-03-20 15:50:20 +0100

    feat: remove unused api route

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,22 +1,12 @@
 import { Hono } from "hono";
-import {zValidator} from "@hono/zod-validator";
-import {z} from "zod";
-import {setResponseInRedis} from "../util/redis.ts";
 import points from "./points.ts";
-import checkWords from "./checkWords.ts";
+import check from "./check.ts";
 
 const api = new Hono()
 
 const routes = api
-    .post('/response', zValidator('json', z.object({
-        response: z.boolean()
-    })), async (ctx) => {
-        const { response }= ctx.req.valid('json');
-        const result = await setResponseInRedis(response);
-        return ctx.json(result);
-    })
     .route('/', points)
-    .route('/', checkWords)
+    .route('/', check)
 
 export type APIType = typeof routes;
 export default api;
