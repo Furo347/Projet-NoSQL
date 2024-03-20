045eb0c9dcf4409ade9958e0ec1aaf71f448d3d7
DawnWT <dwnssb@gmail.com>
2024-03-20 15:50:45 +0100

    feat: fix useScoreboardData

--- src/hooks/useScoreboardData.ts
+++ src/hooks/useScoreboardData.ts
@@ -4,14 +4,10 @@ import { apiClient } from "../client";
 export type ScoresDto = Parameters<typeof apiClient.setPoints[":name"]["$post"]>[0]["param"];
 export type ScoresReturn = Awaited<ReturnType<Awaited<ReturnType<typeof apiClient.setPoints[":name"]["$post"]>>['json']>>;
 
-export function useScoreboardData(name: string | null) {
+export function useScoreboardData() {
   const [scores, setScores] = useState<ScoresReturn[]>([])
 
   useEffect(() => {
-    if (!name) {
-      return
-    }
-
     apiClient.scoreboard.$get()
       .then((res) => res.json())
       .then((res) => {
@@ -22,7 +18,7 @@ export function useScoreboardData(name: string | null) {
         console.error('Error fetching scoreboard data:', error);
         return;
       })
-  }, [name])
+  }, [])
 
   return scores
 }
