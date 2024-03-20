d83d4217532c36e288fd381e330bfee139725825
DawnWT <dwnssb@gmail.com>
2024-03-20 15:56:03 +0100

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
-import PlayerNameDialog from './components/identification';
-import './App.css';
-import BacGrid from './components/grid';
-=======
 import Game from './components/Game'
 import Login from './components/Login/Login'
 import Scoreboard from './components/Scoreboard/Scoreboard';
->>>>>>> feature/tab
 
 function App() {
   return (
